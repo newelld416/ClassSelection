c1f12a68c8672b189dcde2bc55a0655cd420a2ad
Daniel Newell <daniel.newell@amway.com>
2016-10-12 09:53:52 -0400

    Basic Updates

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,8 +1,8 @@
 // src/app/routes.ts
-import {Home} from './components/home';     // ./components/home/index.ts
+import {Home} from './pages/home';
+import {About} from './pages/about';
 
 export default [
     {path: '/', component: Home, name: 'Home'},
-    // Async load a component using Webpack's require with es6-promise-loader
-    {path: '/about', loader: () => require('./components/about')('About'), name: 'About'}
+    {path: '/about', component: About, name: 'About'}
 ];
